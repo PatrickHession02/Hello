1ea952ad2c5be15df15d649f7c92ac12b20ad275
G00394619@atu.ie <g00394619@atu.ie>
2023-09-27 09:40:36 +0100

    HelloController Complete

--- src/main/java/ie/atu/CICD2_lab2a/HelloController.java
+++ src/main/java/ie/atu/CICD2_lab2a/HelloController.java
@@ -1,4 +1,13 @@
 package ie.atu.CICD2_lab2a;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello, World";
+    }
 }
